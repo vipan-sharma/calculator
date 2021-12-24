a14750b3d55b2140eab19b9f90dcbedc5184fddd
Vipan K Sharma <vipansharma4u@gmail.com>
2021-12-24 19:53:42 +0530

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,7 @@
+let a = 10
+let b = 3
+game.splash("Addition of: " + (a + b))
+game.splash("Subtraction of: " + (a - b))
+game.splash("Multiplication is : " + a * b)
+game.splash("Division is : " + a / b)
+game.splash("Reminder is : " + a % b)
